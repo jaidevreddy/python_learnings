ee2b806e4cd9ec2e1497f3937accdb2c1ddada8f
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-02 21:49:30 +0530

    casting

--- casting.py
+++ casting.py
@@ -2,3 +2,11 @@ num1 = 5.8
 int_conv = int(num1)
 print(int_conv)
 print(type(int_conv))
+
+age = input("enter your age: ")
+print(type(age))
+age = int(age)
+new_age = 1 + age
+print(type(new_age))
+new_age = str(new_age)
+print("your going to be " + new_age)
