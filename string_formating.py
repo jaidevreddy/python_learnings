8858c9d8d150c9b7c25fca1d3aef4e2db7b009cc
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-02 22:02:40 +0530

    string formatting old version included

--- string_formating.py
+++ string_formating.py
@@ -2,3 +2,6 @@ color = input("enter your car color: ")
 license = input("enter your car license number: ")
 
 print(f"your car is {color} color and the license number is {license}")
+
+#old version
+print("your car color is {} and the license numer is {}".format(color,license))
