6ac9a512cb7e171d9c01b8ac1cd471bcba107b2c
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-02 22:38:48 +0530

    data types, operations, round methons and string formating

--- percentage.py
+++ percentage.py
@@ -1,4 +1,4 @@
 name = input("Enter Your name: ")
-sales = float(input("enter your sales for the month: "))
+sales = int(input("enter your sales for the month: "))
 
 print(f"hey {name} you this months sales is ${sales} and your commision for the month is ${round(sales*13/100,2)}")
