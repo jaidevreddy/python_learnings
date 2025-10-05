efed955daaa22a15288813ec92e64b2f9929dafa
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-05 21:16:29 +0530

    string properties

---
+++ stringproperties.py
@@ -0,0 +1,16 @@
+n1 = "ga"
+n2 = "rry"
+print(n1+n2)
+print(n1*10)
+
+poem = """Thousand little white fishes
+as if boiled 
+the color of water"""
+print(poem)
+
+print("water" in poem)
+print("sun" not in poem)
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+size = len(letters)
+print(size)
