e6a423d1d0fae0bd296f7fad957d2687b7619a69
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-05 21:24:46 +0530

    lists properties

--- lists.py
+++ lists.py
@@ -1,6 +1,15 @@
-list = [1,2,3,4,"a","b",5,'c']
-list2 = list.copy()
-list2.append("hello")
-list2.pop(2)
-print(list)
-print(list2)
+list = ["a","b","c"]
+
+lenght = len(list)
+print(lenght)
+
+result = list[0]
+res = list[0:2]
+res1 = list[::-1]
+res2 = list[:2]
+res3 = list[::-2]
+print(result)
+print(res)
+print(res1)
+print(res2)
+print(res3)
