78d0332a0ac16d8b3a6d227cde69becd7171a502
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-06 20:57:04 +0530

    word count done using split method

--- text_analyser.py
+++ text_analyser.py
@@ -10,5 +10,11 @@ print("how many times each of the chosen letters appears:")
 print(f'the letter "{letters[0]}" repeats: "{text.count(letters[0])}"')
 print(f'the letter "{letters[1]}" repeats: "{text.count(letters[1])}"')
 print(f'the letter "{letters[2]}" repeats: "{text.count(letters[2])}"')
+print('\n')
+
+words = text.split()
+print(f'Total number of word in the the text is {len(words)}')
+print('\n')
+
 
 
