7c2d6dcaa830b32f2bb4d72aba5d25af3afe199c
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-06 19:12:54 +0530

    everything about sets

--- sets.py
+++ sets.py
@@ -3,3 +3,22 @@ otherset = {1,2,3,4,5}
 
 print(myset)
 print(otherset)
+
+sets = set([1,2,3,4,5,1,1,1,1])
+print(sets)
+
+tupleset = set([1,2,3,(1,2,3)])
+print(tupleset)
+print(2 in tupleset)
+
+s1 = {1,2,3}
+s2 = {3,4,5}
+s3 = s1.union(s2)
+s3.add("a")
+s3.remove(3)
+s3.discard(6)
+print(s3)
+
+popset = {'a','b','c'}
+popset.pop()
+print(popset)
