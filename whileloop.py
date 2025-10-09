2af1b339c36f5abfd1afe18d467d6ed342225666
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-09 22:57:00 +0530

    while loops

--- whileloop.py
+++ whileloop.py
@@ -2,5 +2,15 @@ coins = 5
 
 while coins > 0:
     print(f'i have {coins} coins')
-    coins = coins -1 
+    coins -= 1
+else:
+    print("you have no more coins left")
 
+answer = 'y'
+
+while answer =='y':
+    answer = input("do you want to continue (y/n)")
+    if answer == 'y':
+        print("you deicded to continue") 
+else:
+    print("you decide to end")   
