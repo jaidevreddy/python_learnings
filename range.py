9ff37fe84e15549839abd2312aca62bb9d380086
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-10 21:30:17 +0530

    range function

--- range.py
+++ range.py
@@ -5,3 +5,18 @@ print("\n")
 
 for i in range (20,31,2):
     print(i)
+
+mylist = list(range(1,101))
+print(mylist)
+
+sum_squares = 0
+
+for i in range(1,16):
+    i = i**2
+    sum_squares = sum_squares + i
+    print(i)
+
+print(sum_squares)
+
+
+
