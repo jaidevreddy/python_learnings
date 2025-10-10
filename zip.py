39e6050ac920c3f5437df68ad0a013f93b267601
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-10 22:32:34 +0530

    zip function

--- zip.py
+++ zip.py
@@ -1,6 +1,19 @@
 name = ['Hanna','Bruce','Tony']
-age = [65,29,42]
+age = [65,29,42,55]
+city = ['New York','London','Berlin']
 
-combine = list(zip(name,age))
+combine = list(zip(name,age,city))
 
 print(combine)
+
+for name , age , city in combine:
+    print(f'{name} is {age} and lives in {city}')
+
+capitals = ["Berlin", "Tokyo", "Paris", "Helsinki", "Ottawa", "Canberra"]
+countries = ["Germany", "Japan", "France", "Finland", "Canada", "Australia"]
+
+combine = list(zip(capitals,countries))
+
+for capitals, countries in combine:
+    print(f"The capital of {countries} is {capitals}")
+
