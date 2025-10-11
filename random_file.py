3664631e63e960d630dbc06ef3b40cce65884c06
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-11 21:42:37 +0530

    python libraries

--- random_file.py
+++ random_file.py
@@ -1,5 +1,29 @@
-from random import randint
+from random import *
 
-random = randint(1,50)
-
+random0 = randint(1,50)
 print(random)
+
+random1 = round(uniform(1,50),1)
+print(random1)
+
+random2 = round(random(),2)
+print(random2)
+
+color = ['black', 'blue', 'red', 'green']
+print(choice(color))
+
+listnum = list(range(5,50,5))
+print(listnum)
+shuffle(listnum)
+print(listnum)
+
+
+names = ['aegon','barathion','cersie','drogon','emilia']
+shuffle(names)
+picked = names[:3]
+print(picked)
+
+for i in picked:
+    names.remove(i)
+
+print(names)
