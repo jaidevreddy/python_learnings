3560f285ab96c10d713818a61975a9de1b6ecad4
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-11 20:51:52 +0530

    min max functions

--- minmax.py
+++ minmax.py
@@ -1,3 +1,18 @@
 mylist = [58,38,95,100,48,101]
 
 print(f"the least is {min(mylist)} and max is {max(mylist)}")
+
+names =['jhon', 'paul', 'alicia','charles']
+
+print(min(names),  max(names))
+
+name = 'carlOs'
+
+print(min(name))
+
+dictionary_ages = {"Tony":55, "Paulie":42, "Meadow":78, "Vito":44, "Ralph":24, "Sarah":35, "Evan":19, "Jean":2 ,"Ned":49}
+
+minimum_age = min(dictionary_ages.values())
+last_name = max(dictionary_ages)
+
+print(minimum_age, last_name)
