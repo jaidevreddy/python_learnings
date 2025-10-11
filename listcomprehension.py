4a5fc7f4981e2739353929f2f8f4fbafc56e798f
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-11 22:34:30 +0530

    list comprehensions

--- listcomprehension.py
+++ listcomprehension.py
@@ -3,3 +3,17 @@ word = input('enter your name: ')
 nameslist = [letter for letter in word]
 
 print(nameslist)
+
+
+numbers = range(0,21,2)
+even_numbers = [num for num in numbers]
+print(even_numbers)
+
+mylist = [n if n*2 >10 else 'no' for n in numbers ]
+print(mylist)
+
+feet = [10,20,30,40,50]
+meters = [ f * 0.3048 for f in feet]
+
+print(meters)
+
