a8affdf6d595c9dcbe071c8ba07a6a5bbd606736
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-12 19:42:57 +0530

    matches

--- matches.py
+++ matches.py
@@ -7,3 +7,24 @@ match series:
         print("product 2")
     case _:
         print("no product found")
+
+
+name = input("enter your name: ")
+age = input("enter your age:")
+client = {"name":name, "age":age}
+
+movie = {"title":'matrix', "cast":{"mainlead":"keanu reeves", "director":"lana and lily"}}
+
+items = [client, movie,"book"]
+
+for i in items:
+    match i:
+        case {"name":name, 
+              "age":age}:
+            print(name,age)
+        case {"title":title, 
+              "cast":{"mainlead":mainlead, 
+                      "director":director}}:
+            print(title, mainlead, director)
+        case _:
+            print("there is nothing like this")
