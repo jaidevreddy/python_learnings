3931df8d9eaa7ad31b65b5acadcc150ed86c3bd0
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-13 19:35:32 +0530

    functions

--- createfunctions.py
+++ createfunctions.py
@@ -13,6 +13,6 @@ greetperson(ename)
 def square(number):
     print(f"answer: {number**2}")
 
-num = int(input("enter your number: "))
+num = int(input("enter your number for finding the power of 2: "))
 square(num)
 
