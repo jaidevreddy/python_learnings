ee7379330341ee12c9f48ce22e9e71475b56ce77
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-15 19:07:41 +0530

    dynamic function

--- dynamic_functions.py
+++ dynamic_functions.py
@@ -1,6 +1,21 @@
-def check(num):
-    value = num in range (100,1001)
+def check(number):
+    value = number in range (100,1001)
     return value
 
-result = check(658)
+sum = 949+30
+result = check(sum)
+print(result)
+
+def check3(num):
+    for n in num:
+        if n in range(100,1000):
+            return True
+        else:
+            pass
+    
+    else:
+        return False
+
+
+result = check3([1000,100,2342,5836])
 print(result)
