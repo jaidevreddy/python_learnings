22244d97324e7a06c2bece6819e4d8db35dd8c6c
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-16 22:29:47 +0530

    functions

--- createfunctions.py
+++ createfunctions.py
@@ -1,18 +1,30 @@
-def greet():
-    print("hello ")
+# def greet():
+#     print("hello ")
 
 
-greet()
+# greet()
 
-def greetperson(name):
-    print("Hello " + name)
+# def greetperson(name):
+#     print("Hello " + name)
 
-ename = input("enter your name: ")
-greetperson(ename)
+# ename = input("enter your name: ")
+# greetperson(ename)
 
-def square(number):
-    print(f"answer: {number**2}")
+# def square(number):
+#     print(f"answer: {number**2}")
 
-num = int(input("enter number"))
-square(num)
+# num = int(input("enter number"))
+# square(num)
 
+
+coffee_price = [('cappuccino',1.5),
+                ('espreso',1.2),
+                ('mocha',1.9)]
+
+def most_expensive_coffee(value):
+    highest_price = max(value)
+    return highest_price
+
+res = most_expensive_coffee(coffee_price)
+
+print (res)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,17 @@
-20
+def greet():
+#     print("hello ")
+
+
+# greet()
+
+# def greetperson(name):
+#     print("Hello " + name)
+
+# ename = input("enter your name: ")
+# greetperson(ename)
+
+# def square(number):
+#     print(f"answer: {number**2}")
+
+# num = int(input("enter number"))
+# square(num)
