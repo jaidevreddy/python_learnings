ef3a22db951754d087c16aa7f8ddbc90fc9719d7
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-17 14:00:58 +0530

    functions interactions

--- function_interaction.py
+++ function_interaction.py
@@ -1,28 +1,28 @@
 from random import *
 
-# sticks = ['-','--','---','----']
+sticks = ['-','--','---','----']
 
-# def mix(shuf):
-#     shuffle(shuf)
-#     return shuf
+def mix(shuf):
+    shuffle(shuf)
+    return shuf
 
-# def try_your_luck():
-#     a_try = ''
-#     while a_try not in ['1','2','3','4']:
-#         a_try = input("choose a number: ")
+def try_your_luck():
+    a_try = ''
+    while a_try not in ['1','2','3','4']:
+        a_try = input("choose a number: ")
     
-#     return int(a_try)
+    return int(a_try)
     
-# def check_stick(list1,selected):
-#     if list1[selected-1] == '-':
-#         print("your turn to do the task")
-#     else:
-#         print(f"you picked {list1[selected-1]} and no work for you")
+def check_stick(list1,selected):
+    if list1[selected-1] == '-':
+        print("your turn to do the task")
+    else:
+        print(f"you picked {list1[selected-1]} and no work for you")
 
 
-# stick_mix = mix(sticks)
-# choose_stick = try_your_luck()
-# you_got = check_stick(stick_mix,choose_stick)
+stick_mix = mix(sticks)
+choose_stick = try_your_luck()
+you_got = check_stick(stick_mix,choose_stick)
 
 
 def throw_dice():
