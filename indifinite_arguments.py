885e8ff4cfcf037e1c1117ce08731c030442ab95
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-21 22:42:28 +0530

    indefinite funtions

--- indifinite_arguments.py
+++ indifinite_arguments.py
@@ -1,51 +1,51 @@
-def sums(*args):
-    total = 0
+# def sums(*args):
+#     total = 0
 
-    for num in args:
-        total = total + num
+#     for num in args:
+#         total = total + num
     
-    return total
+#     return total
 
-print(sums(6,7,8))
+# print(sums(6,7,8))
 
-print(sums(6,7,8))
+# print(sums(6,7,8))
 
-def sums2(*args):
-    total = sum(args)
+# def sums2(*args):
+#     total = sum(args)
     
-    return total
+#     return total
 
-print(sums2(6,7,8))
+# print(sums2(6,7,8))
 
-def sum_squares(*args):
+# def sum_squares(*args):
 
-    total = 0
+#     total = 0
 
-    for sq in args:
-        total = sq**2 + total
+#     for sq in args:
+#         total = sq**2 + total
 
-        return total
+#         return total
     
-print(sum_squares(5,2,5,3,5,224,3,325,44))
+# print(sum_squares(5,2,5,3,5,224,3,325,44))
 
-def absoulte_sum(*args):
-    total = 0
+# def absoulte_sum(*args):
+#     total = 0
 
-    for num in args:
-        if num >= 0:
-            total = total + num
-        elif num < 0:
-            num = num * -1
-            total = total + num
+#     for num in args:
+#         if num >= 0:
+#             total = total + num
+#         elif num < 0:
+#             num = num * -1
+#             total = total + num
     
-    return total
+#     return total
 
-print(absoulte_sum(1,-2,4,-435,-500))
+# print(absoulte_sum(1,-2,4,-435,-500))
 
 
-def personal_numbers(name,*args):
-    total = sum(args)
+# def personal_numbers(name,*args):
+#     total = sum(args)
 
-    return f"{name}, the sum of your numbers is {total}"
+#     return f"{name}, the sum of your numbers is {total}"
 
-print(personal_numbers('name',1,23,4,4))
+# print(personal_numbers('name',1,23,4,4))
