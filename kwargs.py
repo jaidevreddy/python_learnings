ae8763888f012fc2ea0e7c18037e69c5ab88de82
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-22 23:26:03 +0530

    indefinite functions (**kwargs)

--- kwargs.py
+++ kwargs.py
@@ -11,3 +11,18 @@ def a_sum(**kwargs):
 
 
 print(a_sum(x=1,y=2,z=3))
+
+
+def test(num1,num2,*args,**kwargs):
+
+    print(f"the first value is {num1}")
+    print(f"the second value is {num2}")
+
+    for arg in args:
+        print(f"{arg}")
+    
+    for key, value in kwargs.items():
+        print(f"{key} = {value}")
+
+
+test(1,2,100,200,300,400,500,x='one',y='two',z='three')
