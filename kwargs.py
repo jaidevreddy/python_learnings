3be26c9f060a721f8f292d15266fdfe0a58edc54
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-22 23:18:29 +0530

    indefinite function (**kwargs)

--- kwargs.py
+++ kwargs.py
@@ -1,9 +1,13 @@
 def a_sum(**kwargs):
 
+    total = 0
+
     for key, value in kwargs.items():
         print(f"{key} = {value}")
 
+        total = total + value
+    
+    return total
 
 
-
-a_sum(x=1,y=2,z=3)
+print(a_sum(x=1,y=2,z=3))
