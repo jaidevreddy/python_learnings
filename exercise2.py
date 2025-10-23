c1bb4c5274876049e28b8b90ac9795d6ca4aca61
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-23 23:25:11 +0530

    exercise 2

--- exercise2.py
+++ exercise2.py
@@ -1,13 +1,20 @@
 def exercise2(word):
 
-    word_list = [word]
-    word_list.sort()
+    word_list = []
     final_list = []
 
-    for i in word_list:
-        if i not in word_list:
-            final_list.append(i)
-            output = " ".join(final_list)
-            return output
-        
+    for i in word:
+        word_list.append(i)
     
+    word_list.sort()
+
+    for item in word_list:
+
+        if item not in final_list:
+            final_list.append(item)
+
+    output = " ".join(final_list)
+    return output
+
+    
+print(exercise2('alphabets'))
