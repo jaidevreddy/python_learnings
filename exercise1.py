ed45bcbc43ddd0347fa060b45483b3f277eceedd
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-23 22:53:44 +0530

    exercise 1

---
+++ exercise1.py
@@ -0,0 +1,22 @@
+def return_distincts(num1,num2,num3):
+
+    val_list = [num1,num2,num3]
+
+    total = num1 + num2 + num3
+
+    if total > 15:
+        max_val = max(val_list)
+        return max_val
+    elif total < 10:
+        least_val = min(val_list)
+        return least_val
+    elif total >= 10 and total <=15:
+        sorted_list = val_list.sort(reverse=False)
+        sorted_list = sorted_list.remove(max(sorted_list))
+        intermediate_val = val_list[0]
+        return intermediate_val
+
+
+print(return_distincts(6,2,3))
+
+    
