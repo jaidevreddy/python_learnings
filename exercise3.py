d5b12d1fdd1f39567de9d16c3b50cba0420dffd0
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-24 22:57:05 +0530

    exercise 3

--- exercise3.py
+++ exercise3.py
@@ -1,3 +1,16 @@
 def exercise3(*args):
 
+    current_num = None
+
+    for i in args:
+
+        if current_num == 0 and i == 0:
+            return True
+        
+        current_num = i
     
+    return False
+
+
+print(exercise3(1,2,3,4,4,0))
+
