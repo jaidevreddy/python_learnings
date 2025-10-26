d324ab35b813c2769d08f5f9f6879e3786f1a2ed
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-26 20:15:35 +0530

    hangman game

--- hangman.py
+++ hangman.py
@@ -8,3 +8,17 @@ def choose(list1):
     letters = list(word)
 
     return letters
+
+def thegame(list1):
+
+    life = 6
+
+    while life > 0:
+
+        for i in list1:
+            print(f"your have {life} lifes, category fruits")
+
+            underscore = "_" * (len(list1))
+
+            print(underscore)
+
