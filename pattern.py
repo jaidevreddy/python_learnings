1026f178be330dae9fb92f5ea2fdf5b40e07c48c
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-27 21:08:53 +0530

    reverse right angle triangle with numbers

--- pattern.py
+++ pattern.py
@@ -24,3 +24,18 @@ for i in range (1,row+1):
         print('*',end=' ')
     
     print("")
+
+
+print("")
+print("reverse right angle triangle with numbers")
+print('')
+
+n =5 
+k =5
+
+for i in range(0,n+1):
+
+    for j in range(k-i,0,-1):
+        print(j,end=' ')
+    
+    print("")
