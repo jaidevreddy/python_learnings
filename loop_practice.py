20061790d1d62ae1d0bec023469ccb6bb9326230
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-10-28 23:50:54 +0530

    practice question 6

--- loop_practice.py
+++ loop_practice.py
@@ -62,3 +62,12 @@ for i in range(1,6):
     print(i)
     if i == 5:
         print('Done!')
+
+
+print("")
+
+# Print elements from a given list present at odd index positions
+
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+print(my_list[1::2])
