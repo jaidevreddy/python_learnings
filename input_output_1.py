2f97bc968efdddc17f7346c288dd460d72082f59
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-11-01 23:23:55 +0530

    file manipulation, open(),read() and other methods

--- input_output_1.py
+++ input_output_1.py
@@ -1,17 +1,36 @@
 my_file = open('test.txt')
 
-# print(my_file.read())
+print(my_file.read())
 
-# one_line = my_file.readline()
-# print(one_line)
+print("")
+my_file.close()
 
-# for i in my_file:
+my_file = open('test.txt')
 
-#     print(i)
+one_line = my_file.readline()
+print(one_line)
 
-# all = my_file.readlines()
-# all = all.pop()
-# print(all)
+print("")
+my_file.close()
+
+my_file = open('test.txt')
+for i in my_file:
+
+    print(i)
+print("")
+my_file.close()
+
+my_file = open('test.txt')
+all = my_file.readlines()
+print(all)
+all_del = all.pop()
+print(all_del)
+print(all)
+
+print("")
+my_file.close()
+
+my_file = open('test.txt')
 
 j = 0 
 
@@ -20,8 +39,6 @@ for i in my_file:
     j = j + 1
     if j == 2:
         print(i)
-
-    
 
 
 my_file.close()
