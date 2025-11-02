8752a52f144fd26245b02425ce48ea99bdd4d2ba
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-11-02 16:13:51 +0530

    file manipulation

---
+++ write_files.py
@@ -0,0 +1,17 @@
+my_file = open('test2.xlsx','w')
+
+my_file.write("I am the new text")
+
+my_file.close()
+
+my_file = open('test1.txt','a')
+
+my_file.write("hello starting and where it ended")
+
+my_file.close()
+
+my_file = open('test1.txt','r')
+
+my_file.write("hello")
+
+my_file.close()
