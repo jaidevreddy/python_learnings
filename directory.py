04be705d638a6044f10d59bf12e9a591ae30f7ff
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-11-04 21:53:35 +0530

    directories

--- directory.py
+++ directory.py
@@ -3,3 +3,12 @@ import os
 path = os.getcwd()
 
 print(path)
+
+path2 = os.chdir('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/sem 7/GCP/file_encryption') #this path is from different directory
+
+file = open('tesst.txt','r')
+
+print(file.read())
+
+path3 = os.makedirs('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/python/test_dir')
+
