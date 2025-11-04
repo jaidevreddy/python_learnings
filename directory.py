f011dbb5fb047fe235dae92a5cdd393f5308e989
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2025-11-04 22:54:41 +0530

    directories

--- directory.py
+++ directory.py
@@ -1,23 +1,40 @@
-import os
+# import os
 
-path = os.getcwd()
+# path = os.getcwd()
 
-print(path)
+# print(path)
 
-path2 = os.chdir('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/sem 7/GCP/file_encryption') #this path is from different directory
+# path2 = os.chdir('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/sem 7/GCP/file_encryption') #this path is from different directory
 
-file = open('tesst.txt','r')
+# file = open('tesst.txt','r')
 
-print(file.read())
+# print(file.read())
 
-# path3 = os.makedirs('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/python/test_dir')
-path3_alt = ('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/python/test_dir')
+# # path3 = os.makedirs('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/python/test_dir')
+# path3_alt = ('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/python/test_dir')
 
-# element = os.path.basename(path3_alt)
-# element = os.path.dirname(path3_alt)
+# # element = os.path.basename(path3_alt)
+# # element = os.path.dirname(path3_alt)
 
-element = os.path.split(path3_alt)
+# element = os.path.split(path3_alt)
 
-print(element)
+# print(element)
 
-os.rmdir('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/python/test_dir')
+# os.rmdir('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/python/test_dir')
+
+from pathlib import Path
+
+folder = Path('/Users/jaidevreddy/Documents/Documents - Jaidev’s MacBook Pro/python') / 'test.txt'
+
+my_file = open(folder,'r')
+
+print(my_file.read())
+
+
+
+
+
+
+
+
+
