0f16bf2ef4f2da671ad74367a023b9183849f24e
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-03-12 00:22:16 +0530

    mathematical operations

---
+++ mathematical_operations.py
@@ -0,0 +1,15 @@
+num1 = 874
+num2 = 27
+result = num1 // num2
+print(result)
+
+
+num1 = 456
+num2 = 33
+result = num1 % num2
+print(result)
+
+
+num1 = 783
+sqrt = num1 ** 0.5
+print(round(sqrt,3))
