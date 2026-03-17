4eeb8c5ea566270e742b5715d3d6e661c88d8e8a
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-03-17 23:48:59 +0530

    slicing

---
+++ slicing.py
@@ -0,0 +1,19 @@
+sentence = "Controlling complexity is the essence of programming"
+
+result = sentence[0:11]
+
+print(result)
+
+
+sentence = "Never trust a computer you can't throw out a window"
+
+result = sentence[8::3]
+
+print(result)
+
+
+sentence = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+
+reverse = sentence[::-1]
+
+print(reverse)
