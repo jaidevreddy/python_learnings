998fdc1a7b8cbc1c8f8838ec93b52abe1b8d1160
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-03-17 23:43:48 +0530

    indexing

--- index.py
+++ index.py
@@ -2,3 +2,11 @@ word = "computer"
 find = word[4]
 
 print(find)
+
+
+word = "computer"
+find = word.rindex("e")
+find1 = word.index("c")
+find2 = word.index("p",1,4)
+
+print(f"{find} {find1} {find2}")
