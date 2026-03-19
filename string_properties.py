08148d08ea61de3110e0546752efef1d65d27159
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-03-19 21:36:26 +0530

    string properties

---
+++ string_properties.py
@@ -0,0 +1,15 @@
+print("Repetition"*15)
+
+print("2"+"3"*5)
+
+
+haiku = """Whitecaps on the bay:
+A broken signboard banging
+In the April wind."""
+
+print("beach" not in haiku)
+
+
+word = "electroencephalographist"
+lenght = len(word)
+print(lenght) 
