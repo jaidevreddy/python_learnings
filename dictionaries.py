7a87eb794b9ab5d044939016ce22a34c76ee6fce
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-03-25 20:14:55 +0530

    disctionaries

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+dic = {"k1":["a","b","c"],"k2":["d","e","f"]}
+
+cap = dic["k2"][1].upper()
+
+print(cap)
+print(dic)
+
+my_dict = {"values_1":{"v1":3,"v2":6},"points":{"points1":9,"points2":[10,300,15]}}
+
+print(my_dict["points"]["points2"][1])
+
+my_dict = {"name":"Karen", "surname":"Jurgens", "age":35, "occupation":"Journalist"}
+
+my_dict['age'] = 36
+my_dict['occupation'] = "Editor"
+my_dict["country"] = "Colombia"
