5500ad971cef68f597a27edd66c799e9af9ee4ea
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-03-25 20:28:32 +0530

    tuples

---
+++ tuples.py
@@ -0,0 +1,16 @@
+tup = (1,"a",3)
+
+x, y, z = tup
+
+print(z)
+
+my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2) 
+
+count = my_tuple.count(2)
+
+print(count)
+
+my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
+
+my_list = list(my_tuple)
+
