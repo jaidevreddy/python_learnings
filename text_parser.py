890c004f2c7a7d9af2cdcb6cc33b3ba28d1b15ca
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-03-27 00:33:33 +0530

    text parser

--- text_parser.py
+++ text_parser.py
@@ -1,29 +1,48 @@
 text = input("enter your text: ").lower()
-letters = input("enter three letters: ").lower()
+letters = []
 
-text_list = text.split()
-letters = tuple(letters)
-letter_1, letter_2, letter_3 = letters
-
-count_1 = text.count(letter_1)
-count_2 = text.count(letter_2)
-count_3 = text.count(letter_3)
+words = text.split()
 
 
-text_count = len(text_list)
+letters.append(input("enter your first letter: "))
+letters.append(input("enter your second letter: "))
+letters.append(input("enter your third letter: "))
+
+count_1 = text.count(letters[0])
+count_2 = text.count(letters[1])
+count_3 = text.count(letters[2])
+
+
+word_count = len(words)
 
 
 first_letter = text[0]
 last_letter = text[-1]
 
 
-text_reverse = text_list
-reverse = " ".join(text_reverse)
+reverse = words[::-1]
+reverse = " ".join(reverse)
 
 python_detect = {True:"Yes", False:"No"}
+check = "python" in words
+final_check = python_detect[check]
 
-text_list = "python" == True 
+
+print(" ")
+print(f"{letters[0]} count is {count_1}")
+print(f"{letters[1]} count is {count_2}")
+print(f"{letters[2]} count is {count_2}")
 
 
-print(first_letter)
-print(last_letter)
+print(" ")
+print(f"word count is {word_count}")
+
+print(" ")
+print(f"the first letter is '{first_letter}' and last letter is '{last_letter}'")
+
+print(" ")
+print(f"reverse order: {reverse}")
+
+print(" ")
+print(f"python exists: {final_check}")
+
