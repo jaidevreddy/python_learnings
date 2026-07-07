f12243fba2996d8000d3a04c136defabd16c433e
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-08 00:52:22 +0530

    print

---
+++ print.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+print("this is a 'python script'")
+
+print(500+55)
+
+print("line1 \nline2 \nline3")
+
+print('How\'s it going')
+
+print("A\tB\tC")
+print("D\tE\tF")
+print("G\tH\tI")
