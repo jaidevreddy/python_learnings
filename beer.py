a410e7fd8958683669d8c354590181d2fd602f19
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-08 01:31:41 +0530

    day one project

---
+++ beer.py
@@ -0,0 +1,2 @@
+print('\n')
+print( "\nBeer name is: " + "\n" + "\n\"" + input("enter you favorite color: ") + " " + input("enter your favorite planet: ") + "\"\n" + "\n" + "Congrats you found your beer name\n")
