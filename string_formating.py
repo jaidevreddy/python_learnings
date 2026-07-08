9249660d270e996d739d205bba0600a9522b01eb
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-09 01:47:33 +0530

    string formating

---
+++ string_formating.py
@@ -0,0 +1,9 @@
+associate_name = "Jesse Pinkman"
+associate_number = 399058
+
+print(f"Dear {associate_name}, your associate number is: {associate_number}")
+
+previous_points = 875
+new_points = 350
+
+print(f"You have earned {new_points} points! In total, you have accumulated {new_points + previous_points} points")
