e9a54a29d99607f56e15f141488f93c6e505b683
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-10 21:49:58 +0530

    indexing

---
+++ index.py
@@ -0,0 +1,11 @@
+word = "computer"
+result = word[4]
+print(result)
+
+sentence = "In theory, theory and practice are the same. In practice, they are not."
+result = sentence.index("practice")
+print(result)
+
+sentence = "In theory, theory and practice are the same. In practice, they are not."
+result = sentence.rindex("practice")
+print(result)
