f30cc0d5cc9c2f3403e8e36e65392d8e6de5fa07
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-11 20:27:57 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,9 @@
+my_set_1 = {1, 2, "three", "four"}
+my_set_2 = {"three", 4, 5} 
+my_set_3 = my_set_1.union(my_set_2)
+
+raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"} 
+raffle.pop()
+
+raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}  
+raffle.add('Gunther')
