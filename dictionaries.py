ff47a4ad814949d7244909dc72bba2ef60c72442
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-11 18:55:12 +0530

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+dict = {"k1":[1,2,3], "k2":{'s1':1,'s2':[1,2,3]}}
+print(dict['k2']['s2'][1])
+
+dic = {'k1':['a','b','c'], 'k2':['d','e','f']}
+print(dic['k2'][1].upper())
+
+my_dict = {"values_1":{"v1":3,"v2":6},"points":{"points1":9,"points2":[10,300,15]}}
+print(my_dict['points']['points2'][1])
+
+my_dict = {"name":"Karen", "surname":"Jurgens", "age":35, "occupation":"Journalist"}
+my_dict['age'] = 36
+my_dict['occupation'] = 'Editor'
+my_dict['country'] = 'Colombia'
