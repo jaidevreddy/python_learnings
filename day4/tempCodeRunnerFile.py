d5961787dcd739857d9b7e494faf9eca0485c3f2
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-15 21:16:35 +0530

    temp file deleted

--- day4/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter a number:"))
-# num2 = int(input("Enter another number:"))
-
-# if num1 > num2:
-#     print(f"{num1} is greater than {num2}")
-# elif num2 > num1:
-#     print(f"{num2} is greater than {num1}")
-# else :
-#     print(f"{num1} and {num2} are equal")
