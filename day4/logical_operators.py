bf6980bef3886297670228fc284d16a5dad2148d
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-15 21:16:12 +0530

    logical operators and decisions making (if/elif/else)

---
+++ day4/logical_operators.py
@@ -0,0 +1,14 @@
+num1 = 36
+num2 = 72/2 
+num3 = 48
+my_bool = num1 > num2 and num1 < num3 
+
+num1 = 36
+num2 = 72/2
+num3 = 48
+my_bool = num1 > num2 or num1 < num3
+
+text = "When something is important enough, you do it even if the odds are against you"
+word1 = "success"
+word2 = "technology"
+my_bool = not (word1 in text) and not (word2 in text)
