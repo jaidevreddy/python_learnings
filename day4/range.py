73b37392e517da49c09f6ef6120937d756c6d4d9
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-07-23 04:19:56 +0530

    range

---
+++ day4/range.py
@@ -0,0 +1,13 @@
+my_list = list(range(2500,2586))
+print(my_list)
+
+my_list = list(range(3,301,3))
+print(my_list)
+
+
+sum_squares = 0
+
+for num in range(1,16):
+    
+    squares = num**2
+    sum_squares += squares
