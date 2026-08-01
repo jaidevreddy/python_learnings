937256842fc7a6fb519a1f373867456f8d8b8e03
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-08-02 01:02:53 +0530

    functions

---
+++ day5/functions.py
@@ -0,0 +1,22 @@
+def greet():
+    
+    print('Hello world!')
+
+
+def welcome(name):
+    
+    print(f"Welcome {name}!")
+    
+    
+name = 'james'
+
+welcome(name)
+
+def square(number):
+    
+    print(number**2)
+
+number = 5
+
+square(number)
+
