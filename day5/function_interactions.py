89ebcde1b62563983a10b166ae34cdd2163221ff
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-08-10 20:54:37 +0530

    interactions between functions

--- day5/function_interactions.py
+++ day5/function_interactions.py
@@ -33,3 +33,58 @@ mixed_list = mix(sticks)
 number = choose_number()
 
 choose_stick(mixed_list,number)
+
+
+# question 1
+
+from random import *
+
+
+def throw_dice():
+    
+    dice = [1,2,3,4,5,6]
+    
+    num_1 = choice(dice)
+    num_2 = choice(dice)
+    
+    return num_1,num_2
+    
+def roll_result(num1,num2):
+    
+    add = num1 + num2
+    
+    if add <=6:
+        return(f"The sum of your dice is {add}. Unfortunate")
+    elif add > 6 and add < 10:
+        return(f"The sum of your dice is {add}. You have a good chance")
+    elif add >=10:
+        return(f"The sum of your dice is {add}. It looks like a winning roll")
+
+
+num1, num2 = throw_dice()
+
+roll_result(num1,num2)
+
+
+#question 2
+
+
+numbers = [1,2,15,7,2]
+
+
+def reduce_list(my_list):
+    
+    unique_list = list(set(my_list))
+    unique_list.remove(max(unique_list))
+    
+    return unique_list
+    
+
+def average(my_list):
+    
+    avg = sum(my_list)/len(my_list)
+    return float(avg)
+    
+a_list = reduce_list(numbers)
+
+avg = average(a_list)
