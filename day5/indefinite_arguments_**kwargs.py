2c33849bd25c4f1d266882480cd7f045500ff30e
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-08-14 19:16:49 +0530

    indefinte arguments **kwargs

--- day5/indefinite_arguments_**kwargs.py
+++ day5/indefinite_arguments_**kwargs.py
@@ -1,2 +1,17 @@
 def number_attributes(**kwargs):
     return len(kwargs)
+
+
+def list_attributes(**kwargs):
+    
+    return list(kwargs.values())
+
+
+
+def describe_person(name,**kwargs):
+    
+    print(f"Characteristics of {name}:")
+    
+    for key,value in kwargs.items():
+        
+        print(f"{key}: {value}")
