29d24f16ee3ada875d894b534380017da893c8f5
K jaidev Shankar reddy <113159217+jaidevreddy@users.noreply.github.com>
2026-08-20 00:58:38 +0530

    exercise_4 bug fix

--- day5/exercise_4.py
+++ day5/exercise_4.py
@@ -1,26 +1,23 @@
+
 def prime_count(number):
+    if number < 2:
+        
+        return 0
 
     prime_number = [2]
     itteration = 3
 
     while itteration <= number:
-
-        for n in range (3,itteration,2):
-
+        for n in range(3, itteration, 2):
             if itteration % n == 0:
                 itteration += 2
-                break 
-            else:
-                prime_number.append(n)
-                itteration += 2
-
+                break
+        else:
+            prime_number.append(itteration)
+            itteration += 2
 
     print(prime_number)
     return len(prime_number)
 
 
-
-
-print(prime_count(15))
-        
-        
+print(prime_count(15))        
